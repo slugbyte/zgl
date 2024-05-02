7123c5b69d953218a4e42930db89c3269d8f1839
Duncan Marsh <slugbyte@slugbyte.com>
2024-05-02 14:13:30 +0800

    patch 3_3 1

--- src/zgl.zig
+++ src/zgl.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const root = @import("root");
-const binding = @import("./binding.zig");
+pub const binding = @import("./binding.zig");
 
 comptime {
     std.testing.refAllDecls(@This());
